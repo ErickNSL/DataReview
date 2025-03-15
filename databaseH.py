87e25376a36bcfdf76e6932bc95411776a5920a3
ErickNSL <erickalvarezsh@gmail.com>
2025-03-14 22:33:36 -0600

    Waching unique values

--- databaseH.py
+++ databaseH.py
@@ -10,32 +10,13 @@ import re
 
 
 #load the data into a df
+busniness = pd.read_csv('2/business.csv')
 economy_df = pd.read_csv('2/economy.csv')
 
 # Check First Row
-print(economy_df.head(1))
+print(busniness.groupby('airline').size().sort_values(ascending=True))
 
-# Function to Fix 'time_taken' Format
-def fix_time_taken(time_str):
-    match = re.match(r'(\d{1,2})h (\d{1,2})m', time_str)  # Allow 1 or 2 digits for hours
-    if match:
-        hour = match.group(1).zfill(2)  # Ensure 2-digit hour
-        minute = match.group(2).zfill(2)  # Ensure 2-digit minute
-        return f"{hour}:{minute}"  # Format as hh:mm
-    return time_str  # Return original if no match
 
-# Apply Fix to 'time_taken'
-economy_df['time_taken'] = economy_df['time_taken'].apply(fix_time_taken)
-
-# Convert to timedelta
-economy_df['time_taken'] = pd.to_timedelta(economy_df['time_taken'] + ':00')  # Append seconds for valid format
-
-# Check Output
-print(economy_df.head(1))
-
-min_time = economy_df["time_taken"].min()
-max_time = economy_df["time_taken"].max()
-print(f"Min Time: {min_time}, Max Time: {max_time}")
 
 
 
